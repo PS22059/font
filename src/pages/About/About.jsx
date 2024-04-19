44d7f441a6cc96e497dd38e2b21a188d82c10c51
PorohitBimal <fbngu@pretreer.com>
2024-04-19 13:09:07 +0700

    update

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -12,8 +12,6 @@ import Footer from "../../components/FooterComponent/FooterComponent";
 const About = () => {
     return (
         <>
-            {/* <Header /> */}
-            {/* <Navbar/> */}
             <div className="about">
                 <img className="bannerAbout" src={bannerAbout} alt="" />
                 <div className="sub-about">
@@ -23,20 +21,21 @@ const About = () => {
                         <li><a href="/about">Giới thiệu</a></li>
                     </ul>
                 </div>
-                <div className="background-slogan">
+                {/* <div className="background-slogan">
                     <div className="slogan">
                         <p className="sub-text">“</p>
                         <p className="text">Looks Good.Run Good.Feels Good.</p>
                         <p className="author">Hannahley</p>
                     </div>
-                </div>
+                </div> */}
                 <div className="sub-background-top"></div>
                 <div className="sub-background-bottom"></div>
                 <div className="about-detail">
                     <div className="about-sports">
                         <div className="left">
-                            <br /><h1>Người ta nói gì về thể thao </h1><br />
-                            <p>Thể thao là sự rèn luyện cho tinh thần và cơ thể. Không có gì quan trọng hơn sự đam mê trong thể thao. Thể thao giúp ta học cách chấp nhận thất bại và khôi phục lại từ những thất bại đó. Thể thao không chỉ là về việc chiến thắng, mà còn là về việc học cách chiến đấu và trưởng thành.</p>
+                            <br /><h1>Người ta nói gì về hạt dinh dưỡng </h1><br />
+                            <p>Các loại hạt cung cấp một hàm lượng vitamin và khoáng chất dồi dào. Thành phần dinh dưỡng của các loại hạt góp phần duy trì và nâng cao sức khỏe cho mọi đối tượng từ trẻ em, người cao tuổi, phụ nữ mang thai, người có bệnh lý nền…
+Trong thành phần của các loại hạt còn chứa nhiều loại chất chống oxy hóa mạnh có tác dụng chống lại các gốc tự do trong tế bào. Gốc tự do chính là thủ phạm làm giảm tổn thương tế bào, gây ra rất nhiều bệnh lý nguy hiểm như: huyết áp, tim mạch, xơ gan, ung thư…</p>
                         </div>
                         <div className="right">
                             <img src={aboutDetail1} alt="" />
@@ -48,8 +47,8 @@ const About = () => {
                             <img src={aboutDetail2} alt="" />
                         </div>
                         <div className="right">
-                            <br /><h1>Sứ mệnh Firefly</h1><br />
-                            <p>Cửa hàng Firefly mong muốn bạn có trải nghiệm dịch vụ tại cửa hàng tốt nhất, luôn luôn lắng nghe những đóng góp ý kiến của các bạn. Thế nên khi bạn gặp vấn đề gì trong quá trình mua hàng tại cửa hàng Firefly xin hãy liên hệ với chúng chúng tôi, để đội ngũ nhân viên hỗ trợ bạn giải quyết vấn đề. Trân trọng cảm ơn bạn đã tin tưởng mua hàng tại cửa hàng Firefly chúng tôi.</p>
+                            <br /><h1>Sứ mệnh Goodnuts</h1><br />
+                            <p>Cửa hàng Goodnuts mong muốn bạn có trải nghiệm dịch vụ tại cửa hàng tốt nhất, luôn luôn lắng nghe những đóng góp ý kiến của các bạn. Thế nên khi bạn gặp vấn đề gì trong quá trình mua hàng tại cửa hàng Goodnuts xin hãy liên hệ với chúng chúng tôi, để đội ngũ nhân viên hỗ trợ bạn giải quyết vấn đề. Trân trọng cảm ơn bạn đã tin tưởng mua hàng tại cửa hàng Goodnuts chúng tôi.</p>
                         </div>
                     </div>
                     
